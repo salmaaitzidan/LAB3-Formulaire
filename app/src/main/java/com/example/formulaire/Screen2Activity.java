7986db66742db60767eb7236d367aa9415552888
Salma Ait Zidan <salma.aitzidan@gmail.com>
2026-04-06 01:04:42 +0200

    Ajout des modifications

--- app/src/main/java/com/example/formulaire/Screen2Activity.java
+++ app/src/main/java/com/example/formulaire/Screen2Activity.java
@@ -4,29 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.TextView;
 import android.content.Intent;
-import android.widget.Toast;
 
 public class Screen2Activity extends AppCompatActivity {
-    private TextView tNom, tEmail, tPhone, tAdresse, tVille;
+    private TextView aNom, aEmail, aPhone, aAdresse, aVille;
 
     private Button btnRetour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_screen2); // relie au XML du récap
+        setContentView(R.layout.activity_screen2);
 
-        tNom = findViewById(R.id.tNom);
-        tEmail = findViewById(R.id.tEmail);
-        tPhone = findViewById(R.id.tPhone);
-        tAdresse = findViewById(R.id.tAdresse);
-        tVille = findViewById(R.id.tVille);
+        aNom = findViewById(R.id.aNom);
+        aEmail = findViewById(R.id.aEmail);
+        aPhone = findViewById(R.id.aPhone);
+        aAdresse = findViewById(R.id.aAdresse);
+        aVille = findViewById(R.id.aVille);
         btnRetour = findViewById(R.id.btnRetour);
 
-        // 1) Récupérer l'Intent qui a lancé cet écran
         Intent intent = getIntent();
 
-        // 2) Extraire les données envoyées depuis MainActivity
         String nom     = intent.getStringExtra("nom");
         String email   = intent.getStringExtra("email");
         String phone   = intent.getStringExtra("phone");
@@ -34,19 +31,13 @@ public class Screen2Activity extends AppCompatActivity {
         String ville   = intent.getStringExtra("ville");
 
 
-        tNom.setText("Nom : " + nom);
-        tEmail.setText("Email : " + email);
-        tPhone.setText("Téléphone : " + phone);
-        tAdresse.setText("Adresse : " + adresse);
-        tVille.setText("Ville : " + ville);
+        aNom.setText("Nom : " + nom);
+        aEmail.setText("Email : " + email);
+        aPhone.setText("Téléphone : " + phone);
+        aAdresse.setText("Adresse : " + adresse);
+        aVille.setText("Ville : " + ville);
 
-
-        // 5) Bouton Retour : fermer cet écran et revenir au précédent
         btnRetour.setOnClickListener(v -> finish());
     }
 
-    // Petite aide : si une valeur est null/vides, retourner "—"
-    private String safe(String s) {
-        return (s == null || s.trim().isEmpty()) ? "—" : s.trim();
-    }
 }
